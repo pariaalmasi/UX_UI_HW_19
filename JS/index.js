136e274026f3dd70b9a0915bd47d4c97ea59b0a0
pariaalmasi <70164423+pariaalmasi@users.noreply.github.com>
2021-01-28 22:40:33 -0500

    v2

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+console.log("this is working -paria");
+
+// $(".title:hover").css("background-color", "red");
+//   
+// }
+
+$(".title").hover(function() {
+    $(this).css("background-color","red")
+  });
